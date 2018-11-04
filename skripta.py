bc3cff7f0857feff7dad02b4edbdccc27f6eed1c
Strmay <jaki.strmi7@gmail.com>
2018-11-04 23:15:04 +0100

    podatki

--- skripta.py
+++ skripta.py
@@ -23,7 +23,6 @@ def download_url_to_string(url):
         return "Ne deluje"
     # nadaljujemo s kodo če ni prišlo do napake
     return r.text
-
     
 def save_string_to_file(text, directory, filename):
     '''Write "text" to the file "filename" located in directory "directory",
@@ -33,7 +32,7 @@ def save_string_to_file(text, directory, filename):
     path = os.path.join(directory, filename)
     with open(path, 'w', encoding='utf-8') as file_out:
         file_out.write(text)
-    return None
+        return None
 
 def save_frontpage():
     text = download_url_to_string(bil_url)
@@ -46,3 +45,47 @@ def read_file_to_string(directory, filename):
     path = os.path.join(directory, filename)
     with open(path, 'r') as file_in:
         return file_in.read()
+
+def page_to_ads(page):
+    '''Split "page" to a list of advertisement blocks.'''
+    ads = []
+    #prvi_ad = []                               #TO ZDEJ ŠTIMA(MAL GRDO THO)
+    #zadnji_ad = []
+    #prvi album je malce posebno predstavljen
+    prvi_ad = [ujem.group(0) for ujem in re.finditer(r'<div class="container container--no-background chart-number-one">(.*?)<div class="ad-container leaderboard leaderboard--top">', page, re.DOTALL)]
+    zadnji_ad = [mec.group(0) for mec in re.finditer(r'data-rank="200"(.*?)WEEKS ON CHART', page, re.DOTALL)]
+    for i in range(2, 200): 
+        for ujemanje in re.finditer(r'(data-rank="{}".*?)data-rank="{}"'.format(i, i+1), page, re.DOTALL):
+            ads.append(ujemanje.group(1))
+    return prvi_ad + ads + zadnji_ad
+
+def get_dict_from_ad_block(block):
+    '''Build a dictionary containing the name, description and price
+    of an ad block.'''
+    rx = re.compile(r'data-rank="(?P<rank>\d+)"',
+                    #r'data-artist="(?P<artist>\w+.*)'
+                    #r'data-ttile="(?P<title>\w+.*)"',
+                    #r'<div class="chart-list-item__last-week">(?P<last_week>\d+)</div>'
+                    #r'<div class="chart-list-item__weeks-at-one">(?P<peek_position>\d+)</div>'
+                    #r'<div class="chart-list-item__weeks-on-chart">(?P<weeks_on_chart>\d+)</div>',
+                    re.DOTALL)
+    data = re.search(rx, block)
+    ad_dict = data.groupdict()
+    return ad_dict
+
+def ads_from_file(directory, filename, page):
+    #sez = page_to_ads(read_file_to_string(directory, filename))
+    ads = []
+    #prvi_ad = []                               #TO ZDEJ ŠTIMA(MAL GRDO THO)
+    #zadnji_ad = []
+    #prvi album je malce posebno predstavljen
+    prvi_ad = [ujem.group(0) for ujem in re.finditer(r'<div class="container container--no-background chart-number-one">(.*?)<div class="ad-container leaderboard leaderboard--top">', page, re.DOTALL)]
+    zadnji_ad = [mec.group(0) for mec in re.finditer(r'data-rank="200"(.*?)WEEKS ON CHART', page, re.DOTALL)]
+    for i in range(2, 200): 
+        for ujemanje in re.finditer(r'(data-rank="{}".*?)data-rank="{}"'.format(i, i+1), page, re.DOTALL):
+            ads.append(ujemanje.group(1))
+    sez2 = []
+    for block in ads:
+        c = get_dict_from_ad_block(block)
+        sez2.append(c)
+    return sez2
